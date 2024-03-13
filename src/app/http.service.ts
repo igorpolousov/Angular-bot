40c57b6c588dff2545e8b22e5d870d23f84c6b8f
Igor Polousov <igorpolousov@gmail.com>
2024-03-13 12:57:44 +0300

    fd

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -15,6 +15,6 @@ export class HttpService{
            
         const body = {name: order.name, age: order.totalSumm};
         const headers = {'Content-Type': 'application/json;charset=utf-8'}
-        return this.http.post("http://89.111.141.36:8000/web-data", body); 
+        return this.http.post("http://89.111.141.36:3000/web-data", body); 
     }
 }
