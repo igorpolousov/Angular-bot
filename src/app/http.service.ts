83544c4e3a23d21ff191c0ce12b94e2892f84125
Igor Polousov <igorpolousov@gmail.com>
2024-03-13 11:18:39 +0300

    headers

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Order} from "./model";
 
 @Injectable()
@@ -8,8 +8,13 @@ export class HttpService{
     constructor(private http: HttpClient){ }
   
     postData(order: Order){
+
+        const myHeaders = new HttpHeaders().set(
+            'Content-Type','application/json;charset=utf-8'
+        );
            
         const body = {name: order.name, age: order.totalSumm};
+        const headers = {'Content-Type': 'application/json;charset=utf-8'}
         return this.http.post("http://89.111.141.36:8000/web-data", body); 
     }
 }
